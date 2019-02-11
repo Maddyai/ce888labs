ef677344e8b8920ca792febf143569e06a7940cc
Maddy <you@example.com>
2019-02-11 18:08:17 +1000

    lab8 done

--- labs/lab2/power.py
+++ labs/lab2/power.py
@@ -5,10 +5,10 @@ def power(sample1, sample2, reps, size, alpha):
     for _ in range(reps):
 		idxs = np.random.randint(len(sample1), size=len(sample1))
 		data1 = [sample1[idx] for idx in idxs]
-        data1_mean = sum(data1)/len(data1)
 
 		idxs = np.random.randint(len(sample2), size=len(sample2))
 		data2 = [sample1[idx] for idx in idxs]
+        data1_mean = sum(data1)/len(data1)
         data2_mean = sum(data2)/len(data2)
 
         val = data1_mean - data2_mean
@@ -20,3 +20,8 @@ def power(sample1, sample2, reps, size, alpha):
     if (pval < 1-alpha):
         return ?    
 
+
+fun1;
+
+
+fun2;
